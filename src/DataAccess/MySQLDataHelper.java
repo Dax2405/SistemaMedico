8f0d52331e30ffe2f03b23efd356f1b225e84d54
Dax <daxnavarrete@gmail.com>
2025-01-27 11:12:55 -0500

    chore: refractor files structure to add mysql driver

---
+++ src/DataAccess/MySQLDataHelper.java
@@ -0,0 +1,36 @@
+package DataAccess;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public abstract class MySQLDataHelper {
+    private static String DB_URL = "jdbc:mysql://93.127.213.145:3306/sistema_medico_epn";
+    private static String DB_USER = System.getenv("DB_USER");
+    private static String DB_PASSWORD = System.getenv("DB_PASSWORD");
+    private static Connection conn = null;
+
+    protected MySQLDataHelper() {
+    }
+
+    protected static synchronized Connection openConnection() throws Exception {
+        try {
+            if (conn == null || conn.isClosed()) {
+                conn = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+            }
+        } catch (SQLException e) {
+            throw new Exception("Failed to connect to the database", e);
+        }
+        return conn;
+    }
+
+    protected static void closeConnection() throws Exception {
+        try {
+            if (conn != null && !conn.isClosed()) {
+                conn.close();
+            }
+        } catch (Exception e) {
+            throw new Exception("Failed to close the database connection", e);
+        }
+    }
+}
