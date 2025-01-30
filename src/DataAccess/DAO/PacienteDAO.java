d55e4c407ead182d8e48a3a151fa3f137137651a
Dax <daxnavarrete@gmail.com>
2025-01-30 14:45:43 -0500

    feat: refractor imports, implement get id by the query response, add readByUsuarioId

--- src/DataAccess/DAO/PacienteDAO.java
+++ src/DataAccess/DAO/PacienteDAO.java
@@ -1,18 +1,14 @@
 package DataAccess.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import DataAccess.DTO.PacienteDTO;
+import DataAccess.MySQLDataHelper;
+import Framework.PoliSaludException;
+
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-
-import DataAccess.MySQLDataHelper;
-import DataAccess.DTO.PacienteDTO;
-import Framework.PoliSaludException;
 
 public class PacienteDAO extends MySQLDataHelper implements IDAO<PacienteDTO> {
     @Override
@@ -115,7 +111,7 @@ public class PacienteDAO extends MySQLDataHelper implements IDAO<PacienteDTO> {
                 + ") VALUES (?, ?, ?, ?, ?, ?, ?)";
         try {
             Connection conn = openConnection();
-            PreparedStatement pstmt = conn.prepareStatement(query);
+            PreparedStatement pstmt = conn.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
             pstmt.setInt(1, entity.getIdUsuario());
             pstmt.setString(2, entity.getNombre());
             pstmt.setString(3, entity.getApellido());
@@ -124,6 +120,12 @@ public class PacienteDAO extends MySQLDataHelper implements IDAO<PacienteDTO> {
             pstmt.setString(6, entity.getFechaNacimiento());
             pstmt.setString(7, entity.getDireccion());
             pstmt.executeUpdate();
+
+            ResultSet rs = pstmt.getGeneratedKeys();
+            while (rs.next()) {
+                entity.setIdPaciente(rs.getInt(1));
+
+            }
             return true;
         } catch (SQLException e) {
             throw new PoliSaludException(e.getMessage(), getClass().getName(), "create()");
@@ -183,5 +185,47 @@ public class PacienteDAO extends MySQLDataHelper implements IDAO<PacienteDTO> {
             throw new PoliSaludException(e.getMessage(), getClass().getName(), "getMaxRow()");
         }
         return 0;
+    }
+
+    public PacienteDTO readByUsuarioId(Integer id) throws Exception {
+        PacienteDTO oS = new PacienteDTO();
+        String query = " SELECT "
+                + "  id_paciente    "
+                + " ,id_usuario    "
+                + " ,nombre        "
+                + " ,apellido      "
+                + " ,codigo_unico  "
+                + " ,telefono      "
+                + " ,fecha_nacimiento "
+                + " ,direccion     "
+                + " ,estado        "
+                + " ,fecha_crea    "
+                + " ,fecha_modifica"
+                + " FROM paciente "
+                + " WHERE estado='A' AND id_usuario = " + id.toString();
+
+        try {
+            Connection conn = openConnection(); // conectar a DB
+            Statement stmt = conn.createStatement(); // CRUD : select * ...
+            ResultSet rs = stmt.executeQuery(query); // ejecutar la consulta
+            while (rs.next()) {
+                oS = new PacienteDTO(
+                        rs.getInt(1), // id_paciente
+                        rs.getInt(2), // id_usuario
+                        rs.getString(3), // nombre
+                        rs.getString(4), // apellido
+                        rs.getString(5), // codigo_unico
+                        rs.getString(6), // telefono
+                        rs.getString(7), // fecha_nacimiento
+                        rs.getString(8), // direccion
+                        rs.getString(9), // estado
+                        rs.getString(10), // fecha_crea
+                        rs.getString(11) // fecha_modifica
+                );
+            }
+        } catch (SQLException e) {
+            throw new PoliSaludException(e.getMessage(), getClass().getName(), "readBy()");
+        }
+        return oS;
     }
 }
