6d58409106a5f09824ad6bbfc330594e120e8b96
Dax <daxnavarrete@gmail.com>
2025-01-30 14:45:43 -0500

    fix: refractor imports and fix fields of the db, add readByOTP

--- src/DataAccess/DAO/AutenticacionOTPDAO.java
+++ src/DataAccess/DAO/AutenticacionOTPDAO.java
@@ -1,29 +1,27 @@
 package DataAccess.DAO;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import DataAccess.DTO.AutenticacionOTPDTO;
+import DataAccess.MySQLDataHelper;
+import Framework.PoliSaludException;
+
+import java.sql.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
-import DataAccess.MySQLDataHelper;
-import DataAccess.DTO.AutenticacionOTPDTO;
-import Framework.PoliSaludException;
 
 public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<AutenticacionOTPDTO> {
     @Override
     public AutenticacionOTPDTO readBy(Integer id) throws Exception {
         AutenticacionOTPDTO oS = new AutenticacionOTPDTO();
         String query = " SELECT "
-                + "  id_autenticacion_otp, "
+                + "  id_autenticacion, "
+                + "  id_usuario, "
                 + "  otp, "
                 + "  otp_expiracion, "
                 + "  fecha_crea "
                 + " FROM autenticacion_otp "
-                + " WHERE id_autenticacion_otp = " + id.toString();
+                + " WHERE id_autenticacion = " + id.toString();
 
         try {
             Connection conn = openConnection();
@@ -31,10 +29,11 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 oS = new AutenticacionOTPDTO(
-                        rs.getInt(1), // id_autenticacion_otp
-                        rs.getString(2), // otp
-                        rs.getString(3), // otp_expiracion
-                        rs.getString(4) // fecha_crea
+                        rs.getInt(1), // id_autenticacion
+                        rs.getInt(2), // id_usuario
+                        rs.getString(3), // otp
+                        rs.getString(4), // otp_expiracion
+                        rs.getString(5) // fecha_crea
                 );
             }
         } catch (SQLException e) {
@@ -47,7 +46,8 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
     public List<AutenticacionOTPDTO> readAll() throws Exception {
         List<AutenticacionOTPDTO> lst = new ArrayList<>();
         String query = " SELECT "
-                + "  id_autenticacion_otp, "
+                + "  id_autenticacion, "
+                + "  id_usuario, "
                 + "  otp, "
                 + "  otp_expiracion, "
                 + "  fecha_crea "
@@ -59,10 +59,11 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
             ResultSet rs = stmt.executeQuery(query);
             while (rs.next()) {
                 AutenticacionOTPDTO s = new AutenticacionOTPDTO(
-                        rs.getInt(1), // id_autenticacion_otp
-                        rs.getString(2), // otp
-                        rs.getString(3), // otp_expiracion
-                        rs.getString(4) // fecha_crea
+                        rs.getInt(1), // id_autenticacion
+                        rs.getInt(2), // id_usuario
+                        rs.getString(3), // otp
+                        rs.getString(4), // otp_expiracion
+                        rs.getString(5) // fecha_crea
                 );
                 lst.add(s);
             }
@@ -75,14 +76,16 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
     @Override
     public boolean create(AutenticacionOTPDTO entity) throws Exception {
         String query = " INSERT INTO autenticacion_otp ("
+                + "id_usuario, "
                 + "otp, "
                 + "otp_expiracion"
-                + ") VALUES (?, ?)";
+                + ") VALUES (?, ?, ?)";
         try {
             Connection conn = openConnection();
             PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setString(1, entity.getOtp());
-            pstmt.setString(2, entity.getOtpExpiracion());
+            pstmt.setInt(1, entity.getIdUsuario());
+            pstmt.setString(2, entity.getOtp());
+            pstmt.setString(3, entity.getOtpExpiracion());
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
@@ -94,14 +97,15 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
     public boolean update(AutenticacionOTPDTO entity) throws Exception {
         DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         LocalDateTime now = LocalDateTime.now();
-        String query = " UPDATE autenticacion_otp SET otp = ?, otp_expiracion = ?, fecha_modifica = ? WHERE id_autenticacion_otp = ?";
+        String query = " UPDATE autenticacion_otp SET id_usuario = ?, otp = ?, otp_expiracion = ?, fecha_modifica = ? WHERE id_autenticacion = ?";
         try {
             Connection conn = openConnection();
             PreparedStatement pstmt = conn.prepareStatement(query);
-            pstmt.setString(1, entity.getOtp());
-            pstmt.setString(2, entity.getOtpExpiracion());
-            pstmt.setString(3, dtf.format(now).toString());
-            pstmt.setInt(4, entity.getIdAutenticacionOTP());
+            pstmt.setInt(1, entity.getIdUsuario());
+            pstmt.setString(2, entity.getOtp());
+            pstmt.setString(3, entity.getOtpExpiracion());
+            pstmt.setString(4, dtf.format(now).toString());
+            pstmt.setInt(5, entity.getIdAutenticacionOTP());
             pstmt.executeUpdate();
             return true;
         } catch (SQLException e) {
@@ -111,7 +115,7 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
 
     @Override
     public boolean delete(Integer id) throws Exception {
-        String query = " DELETE FROM autenticacion_otp WHERE id_autenticacion_otp = ?";
+        String query = " DELETE FROM autenticacion_otp WHERE id_autenticacion = ?";
         try {
             Connection conn = openConnection();
             PreparedStatement pstmt = conn.prepareStatement(query);
@@ -136,5 +140,36 @@ public class AutenticacionOTPDAO extends MySQLDataHelper implements IDAO<Autenti
             throw new PoliSaludException(e.getMessage(), getClass().getName(), "getMaxRow()");
         }
         return 0;
+    }
+
+    public AutenticacionOTPDTO readByOTP(String otp) throws Exception {
+        AutenticacionOTPDTO oS = new AutenticacionOTPDTO();
+        String query = " SELECT "
+                + "  id_autenticacion, "
+                + "  id_usuario, "
+                + "  otp, "
+                + "  otp_expiracion, "
+                + "  fecha_crea "
+                + " FROM autenticacion_otp "
+                + " WHERE otp = " + otp;
+
+        try {
+            Connection conn = openConnection();
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                oS = new AutenticacionOTPDTO(
+                        rs.getInt(1), // id_autenticacion
+                        rs.getInt(2), // id_usuario
+                        rs.getString(3), // otp
+                        rs.getString(4), // otp_expiracion
+                        rs.getString(5) // fecha_crea
+                );
+            }
+        } catch (SQLException e) {
+            throw new PoliSaludException(e.getMessage(), getClass().getName(), "readBy()");
+        }
+        return oS;
+
     }
 }
