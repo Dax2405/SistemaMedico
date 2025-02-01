d8e04d08dd1ea57ba5e657adfd4b984cf3918273
Dax <daxnavarrete@gmail.com>
2025-02-01 11:22:45 -0500

    feat: add main GUI controller with Singleton pattern

---
+++ src/ui/GUI.java
@@ -0,0 +1,85 @@
+package ui;
+
+import javax.swing.*;
+
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.intellijthemes.materialthemeuilite.FlatAtomOneDarkIJTheme;
+
+import BusinessLogic.Entities.Usuario.Medico;
+import BusinessLogic.Entities.Usuario.Paciente;
+
+import java.awt.*;
+
+public class GUI {
+    private static GUI instance;
+    private JFrame frame;
+    private JPanel mainPanel;
+    private CardLayout cardLayout;
+
+    private GUI() {
+        FlatLightLaf.setup();
+        FlatLightLaf.supportsNativeWindowDecorations();
+        try {
+            UIManager.setLookAndFeel(new FlatAtomOneDarkIJTheme());
+        } catch (Exception e) {
+            System.out.println("Error setting theme");
+        }
+        frame = new JFrame("Sistema Medico");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+        cardLayout = new CardLayout();
+        mainPanel = new JPanel(cardLayout);
+        frame.add(mainPanel);
+    }
+
+    public static GUI getInstance() {
+        if (instance == null) {
+            instance = new GUI();
+        }
+        return instance;
+    }
+
+    public void showLoginScreen() {
+        LoginPanel loginPanel = new LoginPanel();
+        mainPanel.add(loginPanel, "login");
+        cardLayout.show(mainPanel, "login");
+        frame.setVisible(true);
+    }
+
+    public void showOTPPanel() {
+        OTPPanel otpPanel = new OTPPanel();
+        mainPanel.add(otpPanel, "otp");
+        cardLayout.show(mainPanel, "otp");
+    }
+
+    public void showPacienteScreen(Paciente paciente) {
+        PacientePanel pacientePanel = new PacientePanel(paciente);
+        mainPanel.add(pacientePanel, "paciente");
+        cardLayout.show(mainPanel, "paciente");
+    }
+
+    public void showRegitrarTurnoScreen(Paciente paciente) {
+        RegistrarTurnoPanel registrarTurnoPanel = new RegistrarTurnoPanel(paciente);
+        mainPanel.add(registrarTurnoPanel, "registrarTurno");
+        cardLayout.show(mainPanel, "registrarTurno");
+    }
+
+    public void showHistoriasClinicasScreen(Paciente paciente) {
+        HistoriasClinicasPanel historiasClinicasPanel = new HistoriasClinicasPanel(paciente);
+        mainPanel.add(historiasClinicasPanel, "historiasClinicas");
+        cardLayout.show(mainPanel, "historiasClinicas");
+    }
+
+    public void showMedicoScreen(Medico medico) {
+        MedicoPanel medicoPanel = new MedicoPanel(medico);
+        mainPanel.add(medicoPanel, "medico");
+        cardLayout.show(mainPanel, "medico");
+    }
+
+    public void showRegistrarPanel() {
+        RegistrarPanel registrarPanel = new RegistrarPanel();
+        mainPanel.add(registrarPanel, "registrar");
+        cardLayout.show(mainPanel, "registrar");
+        frame.setVisible(true);
+    }
+}
