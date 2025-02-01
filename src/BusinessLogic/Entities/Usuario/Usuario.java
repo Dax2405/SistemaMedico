e04033531b0c7f4018687163f30010c87575cac6
Dax <daxnavarrete@gmail.com>
2025-02-01 11:16:47 -0500

    feat: add Usuario entity with it's atributes

---
+++ src/BusinessLogic/Entities/Usuario/Usuario.java
@@ -0,0 +1,59 @@
+package BusinessLogic.Entities.Usuario;
+
+public abstract class Usuario {
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String telefono;
+    private String email;
+    private String contrasena;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContrasena() {
+        return contrasena;
+    }
+
+    public void setContrasena(String contrasena) {
+        this.contrasena = contrasena;
+    }
+
+}
