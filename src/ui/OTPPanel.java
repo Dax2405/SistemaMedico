f473721ea8d7c5c5cad5eb040d415b6fefc6254e
Matthew <169124743+Matthrve@users.noreply.github.com>
2025-02-04 01:03:55 -0500

    Update OTPPanel.java

--- src/ui/OTPPanel.java
+++ src/ui/OTPPanel.java
@@ -1,7 +1,5 @@
 package ui;
 
-import javax.swing.*;
-
 import BusinessLogic.Entities.Autenticacion.AutenticacionOTP;
 import BusinessLogic.Entities.Usuario.Medico;
 import BusinessLogic.Entities.Usuario.Paciente;
@@ -9,29 +7,50 @@ import BusinessLogic.Entities.Usuario.Usuario;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.*;
 
 public class OTPPanel extends JPanel {
     private JTextField otpField;
 
     public OTPPanel() {
-        setLayout(new GridBagLayout());
+        setLayout(new BorderLayout()); 
+        JPanel headerPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        ImageIcon logoIcon = new ImageIcon(new ImageIcon("src/ui/media/SmartTurnLogo.png").getImage().getScaledInstance(150, 140, Image.SCALE_SMOOTH));
+        JLabel logoLabel = new JLabel(logoIcon);
+
+        JLabel titleLabel = new JLabel("SmartTurn");
+        titleLabel.setFont(new Font("Script MT Bold", Font.BOLD, 50)); 
+        titleLabel.setForeground(Color.WHITE); 
+
+        headerPanel.add(logoLabel);
+        headerPanel.add(titleLabel);
+        add(headerPanel, BorderLayout.NORTH);
+
+        JPanel mainPanel = new JPanel(new GridBagLayout());
         GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(5, 5, 5, 5);
 
         JLabel otpLabel = new JLabel("Codigo OTP:");
+        otpLabel.setForeground(Color.WHITE); 
+        otpLabel.setFont(new Font("Cambria", Font.PLAIN, 30));
+        otpLabel.setForeground(Color.WHITE); 
         gbc.gridx = 0;
         gbc.gridy = 0;
-        add(otpLabel, gbc);
+        mainPanel.add(otpLabel, gbc);
 
         otpField = new JTextField(20);
+        otpField.setFont(new Font("Cambria", Font.PLAIN, 25));
         gbc.gridx = 1;
-        add(otpField, gbc);
+        mainPanel.add(otpField, gbc);
 
         JButton loginButton = new JButton("Validar Codigo OTP");
+        loginButton.setBackground(Color.WHITE); // Fondo blanco
+        loginButton.setForeground(new Color(0, 128, 0)); // Letras verdes
+        loginButton.setFont(new Font("Arial", Font.BOLD, 25));
         gbc.gridx = 0;
-        gbc.gridy = 2;
+        gbc.gridy = 1;
         gbc.gridwidth = 2;
-        add(loginButton, gbc);
+        mainPanel.add(loginButton, gbc);
 
         loginButton.addActionListener(new ActionListener() {
             @Override
@@ -43,8 +62,9 @@ public class OTPPanel extends JPanel {
                 }
             }
         });
+        add(mainPanel, BorderLayout.CENTER);
     }
-
+    
     private void validarCodigoOTP() throws Exception {
         String otp = otpField.getText();
         if (otp.isEmpty()) {
